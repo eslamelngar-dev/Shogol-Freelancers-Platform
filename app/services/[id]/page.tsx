19e0b95c301c75969bc5e2ea94662a71011b6cb4
Eslam-Elngar <eslamelngar555@gmail.com>
2026-02-27 08:45:22 +0200

    recommended services

--- app/services/[id]/page.tsx
+++ app/services/[id]/page.tsx
@@ -1,14 +1,21 @@
 "use client";
 import BreadCramp from "@/app/components/UI/BreadCramp";
 import Carousel from "@/app/components/UI/Carousel";
+import ServiceCard from "@/app/components/UI/ServiceCard";
 import { Services } from "@/app/data/Services";
-import { Container } from "@mui/material";
+import { Container, Grid } from "@mui/material";
 import { Star } from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
 import { useParams } from "next/navigation";
 
 export default function ServiceDetails() {
   const { id } = useParams();
   const service = Services.find((s) => s.id === Number(id));
+  const [randomServices] = useState(() => {
+    const filtered = Services.filter((s) => s.id !== Number(id));
+    return filtered.sort(() => Math.random() - 0.5).slice(0, 6);
+  });
 
   return (
     <div className="bg-[#f1f1f1] min-h-screen">
@@ -50,12 +57,24 @@ export default function ServiceDetails() {
                 {service?.description}
               </p>
             </div>
+
+            <div className="flex flex-col rounded-2xl bg-white p-5 justify-center">
+              <p className="text-black p-2">خدمات قد تنال إعجابك</p>
+              <hr className="border-gray-200 p-2" />
+              <Grid container spacing={2}>
+                {randomServices.map((service) => (
+                  <Grid key={service.id} size={{ xs: 12, sm: 6, lg: 4 }}>
+                    <Link href={`/services/${service.id}`}>
+                      <ServiceCard service={service} />
+                    </Link>
+                  </Grid>
+                ))}
+              </Grid>
+            </div>
           </div>
 
           <aside className="w-full lg:w-80 shrink-0 flex flex-col gap-4">
-            <div className="bg-white">
-
-            </div>
+            <div className="bg-white"></div>
           </aside>
         </div>
       </Container>
